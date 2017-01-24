6d596d6e029250dac99336a2459a17ddb0702905
Jieyi <pokkbaby@gmail.com>
2017-01-25 01:19:16 +0900

    Temporally push.

--- app/src/main/kotlin/taiwan/no1/app/ui/customize/CircleImageView.kt
+++ app/src/main/kotlin/taiwan/no1/app/ui/customize/CircleImageView.kt
@@ -1,11 +1,14 @@
 package taiwan.no1.app.ui.customize
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.util.AttributeSet
 import android.widget.ImageView
-import taiwan.no1.app.utilies.AppLog
 
 
 /**
@@ -15,38 +18,61 @@ import taiwan.no1.app.utilies.AppLog
  */
 
 class CircleImageView: ImageView {
-    val imagePaint = Paint()
-    var src_resource: Int = 0
+    private val imagePaint: Paint by lazy {
+        Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            color = Color.GREEN
+            style = Paint.Style.FILL
+        }
+    }
+    private var imgBitmap: Bitmap? = null
 
-    constructor(context: Context): super(context)
-    constructor(context: Context, attrs: AttributeSet): super(context, attrs)
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int): super(context,
-            attrs,
-            defStyleAttr)
+    constructor(context: Context): this(context, null)
+    constructor(context: Context, attrs: AttributeSet?): this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int): this(context,
+            attrs, defStyleAttr, 0)
 
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int): super(context,
+            attrs, defStyleAttr, defStyleRes)
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        AppLog.v(MeasureSpec.getSize(widthMeasureSpec), MeasureSpec.getSize(heightMeasureSpec))
-        AppLog.v(MeasureSpec.getMode(widthMeasureSpec), MeasureSpec.getMode(heightMeasureSpec))
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-//        val bitmap = Bitmap.createBitmap(drawable.intrinsicWidth,
-//                drawable.intrinsicHeight,
-//                Bitmap.Config.ARGB_8888)
-//        val c = Canvas(bitmap)
-//        drawable.setBounds(0, 0, canvas.width, canvas.height)
-//        drawable.draw(c)
-//
-//        val shader = BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
-//        imagePaint.shader = shader
-//        imagePaint.isAntiAlias = true
-//
-//        c.drawCircle((canvas.width / 2).toFloat(),
-//                (canvas.height / 2).toFloat(),
-//                (canvas.height / 2).toFloat(),
-//                imagePaint)
+
+//        imagePaint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+        canvas.drawCircle(width / 2f, height / 2f, Math.min(width / 2f, height / 2f), imagePaint)
+    }
+
+    override fun setImageBitmap(bm: Bitmap?) {
+        super.setImageBitmap(bm)
+        initializeBitmap()
+    }
+
+    override fun setImageDrawable(drawable: Drawable?) {
+        super.setImageDrawable(drawable)
+        initializeBitmap()
+    }
+
+    override fun setImageURI(uri: Uri?) {
+        super.setImageURI(uri)
+        initializeBitmap()
+    }
+
+    override fun setImageResource(resId: Int) {
+        super.setImageResource(resId)
+        initializeBitmap()
+    }
+
+    private fun initializeBitmap() {
+        this.imgBitmap = drawable?.let {
+            Bitmap.createBitmap(it.intrinsicWidth, it.intrinsicHeight, Bitmap.Config.ARGB_8888).apply {
+                Canvas(this).apply {
+                    it.setBounds(0, 0, this.width, this.height)
+                    it.draw(this)
+                }
+            }
+        }
     }
 }
