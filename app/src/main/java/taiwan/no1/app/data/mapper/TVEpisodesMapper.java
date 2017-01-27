0814da202167fd9c8e9990aa71f673a38c55dbea
Weian Cheng <weian@Weiande-MacBook-Pro.local>
2017-01-27 17:20:32 +0800

    implement TV Episodes mapper, but still have an issue.

--- app/src/main/java/taiwan/no1/app/data/mapper/TVEpisodesMapper.java
+++ app/src/main/java/taiwan/no1/app/data/mapper/TVEpisodesMapper.java
@@ -1,11 +1,54 @@
 package taiwan.no1.app.data.mapper;
 
+import android.support.annotation.NonNull;
+
+import com.innahema.collections.query.queriables.Queryable;
+
+import java.util.List;
+
+import javax.inject.Inject;
 import javax.inject.Singleton;
+
+import taiwan.no1.app.data.entities.TVEpisodesEntity;
+import taiwan.no1.app.domain.mapper.IBeanMapper;
+import taiwan.no1.app.mvp.models.MovieDetailModel;
+import taiwan.no1.app.mvp.models.MovieVideosModel;
+import taiwan.no1.app.mvp.models.TVEpisodesModel;
 
 /**
  * Created by weian on 2017/1/21.
  */
 
 @Singleton
-public class TVEpisodesMapper implements <> {
+public class TVEpisodesMapper  implements IBeanMapper<TVEpisodesModel, TVEpisodesEntity> {
+    @Inject MovieVideosMapper tvVideosMapper;
+
+    @Inject
+    public TVEpisodesMapper() {
+    }
+
+    @NonNull
+    @Override
+    public TVEpisodesEntity transformFrom(@NonNull TVEpisodesModel model) {
+        return null;
+    }
+
+    @NonNull
+    @Override
+    public TVEpisodesModel transformTo(@NonNull TVEpisodesEntity entity) {
+        List<MovieVideosModel> tvMovieVideosModel = Queryable.from(entity.getVideos().getResults())
+                .map(this.tvVideosMapper::transformTo)
+                .toList();
+
+        return new TVEpisodesModel(entity.getAir_date(),
+                entity.getEpisode_number(),
+                entity.getName(),
+                entity.getOverview(),
+                entity.getId(),
+                entity.getProduction_code(),
+                entity.getSeason_number(),
+                entity.getStill_path(),
+                entity.getVote_count(),
+                new MovieDetailModel.VideosBean(tvMovieVideosModel));
+    }
 }
