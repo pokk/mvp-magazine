b1eeaff1962b7e8500f967842acf08908165eacf
jieyi <jieyi@mbp.chlorina.local>
2017-02-07 10:22:48 +0900

    Add the parcelable interface.

--- app/src/main/kotlin/taiwan/no1/app/mvp/models/CastListResModel.kt
+++ app/src/main/kotlin/taiwan/no1/app/mvp/models/CastListResModel.kt
@@ -3,6 +3,7 @@ package taiwan.no1.app.mvp.models
 import android.os.Parcel
 import android.os.Parcelable
 import taiwan.no1.app.ui.adapter.viewholder.viewtype.IViewTypeFactory
+import java.util.*
 
 /**
  *
@@ -13,15 +14,13 @@ import taiwan.no1.app.ui.adapter.viewholder.viewtype.IViewTypeFactory
 data class CastListResModel(val page: Int = 0,
                             val total_results: Int = 0,
                             val total_pages: Int = 0,
-                            val results: List<CastBriefBean>? = null) {
-
+                            val results: List<CastBriefBean>? = null): Parcelable {
     data class CastBriefBean(val profile_path: String? = null,
                              val isAdult: Boolean = false,
                              val id: Int = 0,
                              val name: String? = null,
-                             val popularity: Double = 0.toDouble()
-//                             val known_for: List<KnownForBean>? = null
-    ): IVisitable, Parcelable {
+                             val popularity: Double = 0.toDouble(),
+                             val known_for: List<KnownForBean>? = null): IVisitable, Parcelable {
         override fun type(typeFactory: IViewTypeFactory): Int = typeFactory.type(CastBriefBean@ this)
 
         //region Parcelable
@@ -36,7 +35,8 @@ data class CastListResModel(val page: Int = 0,
                 1 == source.readInt(),
                 source.readInt(),
                 source.readString(),
-                source.readDouble())
+                source.readDouble(),
+                source.createTypedArrayList(KnownForBean.CREATOR))
 
         override fun describeContents() = 0
 
@@ -46,6 +46,7 @@ data class CastListResModel(val page: Int = 0,
             dest?.writeInt(id)
             dest?.writeString(name)
             dest?.writeDouble(popularity)
+            dest?.writeTypedList(known_for)
         }
         //endregion
     }
@@ -68,5 +69,81 @@ data class CastListResModel(val page: Int = 0,
                             val name: String? = null,
                             val original_name: String? = null,
                             val genre_ids: List<Int>? = null,
-                            val origin_country: List<String>? = null)
+                            val origin_country: List<String>? = null): Parcelable {
+        //region Parcelable
+        companion object {
+            @JvmField val CREATOR: Parcelable.Creator<KnownForBean> = object: Parcelable.Creator<KnownForBean> {
+                override fun createFromParcel(source: Parcel): KnownForBean = KnownForBean(source)
+                override fun newArray(size: Int): Array<KnownForBean?> = arrayOfNulls(size)
+            }
+        }
+
+        constructor(source: Parcel): this(source.readString(),
+                1 == source.readInt(),
+                source.readString(),
+                source.readString(),
+                source.readString(),
+                source.readInt(),
+                source.readString(),
+                source.readString(),
+                source.readString(),
+                source.readString(),
+                source.readDouble(),
+                source.readInt(),
+                1 == source.readInt(),
+                source.readDouble(),
+                source.readString(),
+                source.readString(),
+                source.readString(),
+                ArrayList<Int>().apply { source.readList(this, Int::class.java.classLoader) },
+                ArrayList<String>().apply { source.readList(this, String::class.java.classLoader) })
+
+        override fun describeContents() = 0
+
+        override fun writeToParcel(dest: Parcel?, flags: Int) {
+            dest?.writeString(poster_path)
+            dest?.writeInt((if (isAdult) 1 else 0))
+            dest?.writeString(overview)
+            dest?.writeString(release_date)
+            dest?.writeString(original_title)
+            dest?.writeInt(id)
+            dest?.writeString(media_type)
+            dest?.writeString(original_language)
+            dest?.writeString(title)
+            dest?.writeString(backdrop_path)
+            dest?.writeDouble(popularity)
+            dest?.writeInt(vote_count)
+            dest?.writeInt((if (isVideo) 1 else 0))
+            dest?.writeDouble(vote_average)
+            dest?.writeString(first_air_date)
+            dest?.writeString(name)
+            dest?.writeString(original_name)
+            dest?.writeList(genre_ids)
+            dest?.writeList(origin_country)
+        }
+        //endregion
+    }
+
+    //region Parcelable
+    companion object {
+        @JvmField val CREATOR: Parcelable.Creator<CastListResModel> = object: Parcelable.Creator<CastListResModel> {
+            override fun createFromParcel(source: Parcel): CastListResModel = CastListResModel(source)
+            override fun newArray(size: Int): Array<CastListResModel?> = arrayOfNulls(size)
+        }
+    }
+
+    constructor(source: Parcel): this(source.readInt(),
+            source.readInt(),
+            source.readInt(),
+            source.createTypedArrayList(CastBriefBean.CREATOR))
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel?, flags: Int) {
+        dest?.writeInt(page)
+        dest?.writeInt(total_results)
+        dest?.writeInt(total_pages)
+        dest?.writeTypedList(results)
+    }
+    //endregion
 }
