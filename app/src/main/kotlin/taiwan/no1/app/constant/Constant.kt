857a3cd93b33b33080ab2ebda7d32724613f8928
Jieyi <pokkbaby@gmail.com>
2017-02-09 20:10:24 +0900

    Add the subtitle of genres in tv list.

--- app/src/main/kotlin/taiwan/no1/app/constant/Constant.kt
+++ app/src/main/kotlin/taiwan/no1/app/constant/Constant.kt
@@ -9,8 +9,30 @@ package taiwan.no1.app.constant
 
 object Constant {
     enum class Gender(val jobName: String) {
-        Male("Actor"),
-        Female("Actress"),
         Unknown("Unknown"),
+        Female("Actress"),
+        Male("Actor"),
+    }
+
+    enum class Genres(val id: Int, val categoryName: String) {
+        Action(28, "Action"),
+        Adventure(12, "Adventure"),
+        Animation(16, "Animation"),
+        Comedy(35, "Comedy"),
+        Crime(80, "Crime"),
+        Documentary(99, "Documentary"),
+        Drama(18, "Drama"),
+        Family(10751, "Family"),
+        Fantasy(14, "Fantasy"),
+        History(36, "History"),
+        Horror(27, "Horror"),
+        Music(10402, "Music"),
+        Mystery(9648, "Mystery"),
+        Romance(10749, "Romance"),
+        Science_Fiction(878, "Science Fiction"),
+        TV_Movie(10770, "TV Movie"),
+        Thriller(53, "Thriller"),
+        War(10752, "War"),
+        Western(37, "Western"),
     }
 }
--- app/src/main/kotlin/taiwan/no1/app/ui/adapter/viewholder/TvListViewHolder.kt
+++ app/src/main/kotlin/taiwan/no1/app/ui/adapter/viewholder/TvListViewHolder.kt
@@ -7,6 +7,7 @@ import android.widget.TextView
 import butterknife.bindView
 import taiwan.no1.app.R
 import taiwan.no1.app.api.config.MovieDBConfig
+import taiwan.no1.app.constant.Constant
 import taiwan.no1.app.mvp.models.TvBriefModel
 import taiwan.no1.app.ui.adapter.CommonRecyclerAdapter
 import taiwan.no1.app.utilies.ViewUtils
@@ -21,17 +22,32 @@ class TvListViewHolder(val view: View): BaseViewHolder(view) {
     private val ivPoster by bindView<ImageView>(R.id.iv_tv_poster)
     private val ivBackdrop by bindView<ImageView>(R.id.iv_tv_backdrop)
     private val tvTitle by bindView<TextView>(R.id.tv_title)
+    private val tvRelease by bindView<TextView>(R.id.tv_release_date)
+    private val tvGenres by bindView<TextView>(R.id.tv_genres)
 
     override fun initView(model: Any, position: Int, adapter: CommonRecyclerAdapter) {
+        this.tvGenres.text = ""
         // Cast the model data type to MovieBriefModel.
         (model as TvBriefModel).let {
+            val maxLengthOfGenres: Int = it.genre_ids?.let {
+                if (it.size >= 3) 3 else it.size
+            } ?: 0
             ViewUtils.loadImageToView(this.mContext.applicationContext,
                     MovieDBConfig.BASE_IMAGE_URL + it.poster_path,
                     this.ivPoster, isFitCenter = false)
             ViewUtils.loadImageToView(this.mContext.applicationContext,
                     MovieDBConfig.BASE_IMAGE_URL + it.backdrop_path,
                     this.ivBackdrop, isFitCenter = false)
+            var genresString: String = ""
+            for (i in 0..maxLengthOfGenres - 1) {
+                Constant.Genres.values().forEach { genresIndex ->
+                    if (it.genre_ids!![i] == genresIndex.id)
+                        genresString += "${genresIndex.categoryName} | "
+                }
+            }
+            this.tvGenres.text = genresString.dropLast(3)
             this.tvTitle.text = it.name
+            this.tvRelease.text = it.first_air_date
             this.item.setOnClickListener {
                 // TODO: 2/5/17 Make the tv detail.
 //                RxBus.get().post(RxbusTag.FRAGMENT_CHILD_NAVIGATOR, hashMapOf(
