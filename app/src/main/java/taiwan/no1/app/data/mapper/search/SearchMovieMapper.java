5dc97687037865aa036f22be96f909118abab46a
Weian Cheng <weian_cheng@htc.com>
2017-02-16 16:02:12 +0800

    implement SearchMovieMapper.

--- app/src/main/java/taiwan/no1/app/data/mapper/search/SearchMovieMapper.java
+++ app/src/main/java/taiwan/no1/app/data/mapper/search/SearchMovieMapper.java
@@ -2,6 +2,12 @@ package taiwan.no1.app.data.mapper.search;
 
 import android.support.annotation.NonNull;
 
+import com.innahema.collections.query.queriables.Queryable;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import taiwan.no1.app.data.entities.search.SearchMovieEntity;
 import taiwan.no1.app.domain.mapper.IBeanMapper;
 import taiwan.no1.app.mvp.models.search.SearchMovieModel;
@@ -14,12 +20,35 @@ public class SearchMovieMapper implements IBeanMapper<SearchMovieModel, SearchMo
     @NonNull
     @Override
     public SearchMovieEntity transformFrom(@NonNull SearchMovieModel model) {
-        return null;
+        throw new Error("No-op");
     }
 
     @NonNull
     @Override
     public SearchMovieModel transformTo(@NonNull SearchMovieEntity entity) {
-        return null;
+        List<SearchMovieModel.ResultsBean> resultsBean = null != entity.getResults() ?
+                Queryable.from(entity.getResults())
+                        .map(data -> new SearchMovieModel.ResultsBean(
+                                data.getPoster_path(),
+                                data.isAdult(),
+                                data.getOverview(),
+                                data.getRelease_date(),
+                                data.getId(),
+                                data.getOriginal_title(),
+                                data.getOriginal_language(),
+                                data.getTitle(),
+                                data.getBackdrop_path(),
+                                data.getPopularity(),
+                                data.getVote_count(),
+                                data.isVideo(),
+                                data.getVote_average(),
+                                null != data.getGenre_ids() ?
+                                        new ArrayList<Integer>(data.getGenre_ids()) : null))
+                        .toList(): Collections.emptyList();
+
+        return new SearchMovieModel(entity.getPage(),
+                entity.getTotal_results(),
+                entity.getTotal_pages(),
+                resultsBean);
     }
 }
