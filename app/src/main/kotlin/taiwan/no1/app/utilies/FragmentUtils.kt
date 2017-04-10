98e488efe348360474c51fff3b4ad88490c1fc42
Jieyi <pokkbaby@gmail.com>
2017-04-11 01:13:23 +0900

    tmp

--- app/src/main/kotlin/taiwan/no1/app/utilies/FragmentUtils.kt
+++ app/src/main/kotlin/taiwan/no1/app/utilies/FragmentUtils.kt
@@ -11,86 +11,74 @@ import java.util.*
  * @since   2/15/17
  */
 
-object FragmentUtils {
-    /**
-     * Adds a [Fragment] to this manager's layout.
-     *
-     * @param manager A [FragmentManager].
-     * @param containerViewId The container view to where add the fragment.
-     * @param fragment The fragment to be added.
-     * @param needBack Set that it can back to previous fragment.
-     * @param sharedElements Shared element objects and ids from layout xml [android:transitionName].
-     * @return the identifier of this transaction's back stack entry.
-     */
-    fun addFragment(manager: FragmentManager,
-                    containerViewId: Int,
-                    fragment: Fragment,
-                    needBack: Boolean = false,
-                    sharedElements: HashMap<View, String>? = null): Int {
-        Preconditions.checkNotNull(containerViewId)
-        Preconditions.checkNotNull(fragment)
-        Preconditions.checkNotNull(needBack)
+/**
+ * Adds a [Fragment] to this manager's layout.
+ *
+ * @param manager A [FragmentManager].
+ * @param containerViewId The container view to where add the fragment.
+ * @param fragment The fragment to be added.
+ * @param needBack Set that it can back to previous fragment.
+ * @param sharedElements Shared element objects and ids from layout xml [android:transitionName].
+ * @return the identifier of this transaction's back stack entry.
+ */
+fun FragmentManager.addFragment(containerViewId: Int,
+                                fragment: Fragment,
+                                needBack: Boolean = false,
+                                sharedElements: HashMap<View, String>? = null): Int {
+    Preconditions.checkNotNull(containerViewId)
+    Preconditions.checkNotNull(fragment)
+    Preconditions.checkNotNull(needBack)
 
-        return manager.beginTransaction().apply {
-            this.replace(containerViewId, fragment, fragment.javaClass.name)
-            sharedElements?.forEach { this.addSharedElement(it.key, it.value) }
-            // https://developer.android.com/training/implementing-navigation/temporal.html#back-fragments
-            if (needBack)
-                this.addToBackStack(fragment.javaClass.name)
-        }.commit()
-    }
+    return this.beginTransaction().also {
+        it.replace(containerViewId, fragment, fragment.javaClass.name)
+        sharedElements?.forEach { k, v -> it.addSharedElement(k, v) }
+        if (needBack)
+            it.addToBackStack(fragment.javaClass.name)
+    }.commit()
+}
 
-    /**
-     * Get a fragment from [FragmentManager] by the tag.
-     *
-     * @param manager A [FragmentManager].
-     * @param tag From [Fragment]'s tag.
-     * @return [Fragment] or null
-     */
-    fun findFragmentByTag(manager: FragmentManager, tag: String): Fragment? = manager.findFragmentByTag(tag)
+/**
+ * Get a fragment from [FragmentManager] by the tag.
+ *
+ * @param manager A [FragmentManager].
+ * @param tag From [Fragment]'s tag.
+ * @return [Fragment] or null
+ */
+inline fun FragmentManager.findFragmentByTag(tag: String): Fragment? = this.findFragmentByTag(tag)
 
-    /**
-     * Pop a [Fragment] from the [FragmentManager].
-     *
-     * @param manager A [FragmentManager].
-     * @return is success to pop a [Fragment].
-     */
-    fun popFragment(manager: FragmentManager): Boolean = if (0 < manager.backStackEntryCount) {
-        manager.popBackStackImmediate()
-        true
-    }
-    else
-        false
+/**
+ * Pop a [Fragment] from the [FragmentManager].
+ *
+ * @param manager A [FragmentManager].
+ * @return is success to pop a [Fragment].
+ */
+inline fun FragmentManager.popFragment(): Boolean = if (0 < this.backStackEntryCount) {
+    this.popBackStackImmediate()
+    true
+}
+else
+    false
 
-    /**
-     * Clear all [Fragment] in the stack.
-     *
-     * @param manager A [FragmentManager].
-     */
-    fun popAllFragment(manager: FragmentManager) {
-        (0..manager.backStackEntryCount - 1).forEach { FragmentUtils.popFragment(manager) }
-    }
+/**
+ * Clear all [Fragment] in the stack.
+ *
+ * @param manager A [FragmentManager].
+ */
+inline fun FragmentManager.popAllFragment() = (0..this.backStackEntryCount - 1).forEach { this.popFragment() }
 
-    fun removeFragment(manager: FragmentManager, fragment: Fragment) {
-        manager.beginTransaction().remove(fragment).commitNow()
-    }
+inline fun FragmentManager.removeFragment(fragment: Fragment) = this.beginTransaction().remove(fragment).commitNow()
 
-    fun removeRecursiveFragment(manager: FragmentManager) {
-        manager.fragments?.forEach {
-            it?.let { f ->
-                it.childFragmentManager?.fragments?.forEach {
-                    it?.let { removeFragment(f.childFragmentManager, it) }
-                }
-            }
+fun FragmentManager.removeRecursiveFragment() = this.fragments?.forEach {
+    it?.let { f ->
+        it.childFragmentManager?.fragments?.forEach {
+            it?.let { f.childFragmentManager.removeFragment(it) }
         }
     }
+}
 
-    fun showAllFragments(manager: FragmentManager) {
-        manager.fragments?.forEach {
-            it?.let {
-                AppLog.v("parent : $it")
-                it.childFragmentManager?.fragments?.forEach { AppLog.d("child!!!: $it") }
-            }
-        }
+fun FragmentManager.showAllFragment() = this.fragments?.forEach {
+    it?.let {
+        AppLog.v("parent : $it")
+        it.childFragmentManager?.fragments?.forEach { AppLog.d("child!!!! : $it") }
     }
 }
